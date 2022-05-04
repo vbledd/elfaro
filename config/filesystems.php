0ceb2eab5fc45176dffe1d1df3d16f4a1ab8490b
vbledd <83681584+vbledd@users.noreply.github.com>
2022-05-03 23:00:59 -0400

    fix storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,11 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+        'public_uploads' => [
+            'driver' => 'local',
+            'root'   => public_path() . '/img',
+            'url' => env('APP_URL').'/public/img',
+        ],
 
     ],
 
